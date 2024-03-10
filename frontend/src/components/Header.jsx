b82ad506d54cea43ef4a543557e2684ee47accb1
addisuanley2010 <addisuanley2005@gmail.com>
2024-03-10 14:25:15 +0300

    tailwind css is added

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import blog1 from "../assets/blog1.jpg";
+import { FaBars } from "react-icons/fa";
+import { AiOutlineClose } from "react-icons/ai";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <nav>
+      <div className="container nav__container">
+        
+        <Link to={"/"} className="nav__logo">
+          <img src={blog1} alt="There is no file at the moment" />
+        </Link>
+        <ul className="nav__menu">
+          <li>
+            <Link to={"/profile"}>Addisu Anley</Link>
+          </li>
 
-export default Header
+          <li>
+            <Link to={"/create"}>Crate Post</Link>
+          </li>
+          <li>
+            <Link to={"/authors"}>Authors</Link>
+          </li>
+          <li>
+            <Link to={"/logout"}>logout</Link>
+          </li>
+        </ul>
+        <button className="nav__toggle-btn">
+          <AiOutlineClose/>
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <h1 className="text-3xl font-bold underline text-red-500">Hello world!</h1>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
